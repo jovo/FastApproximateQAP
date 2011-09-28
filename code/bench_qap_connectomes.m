487336970919f12dfbb014bc71ae021296965968
jovo <joshuav@jhu.edu>
2011-09-28 09:42:58 -0400

    more def edits

--- code/bench_qap_connectomes.m
+++ code/bench_qap_connectomes.m
@@ -1,6 +1,7 @@
 %% connectome times
+clear, clc
 
-load('~/Research/data/EM/c_elegans/c_elegans_chemical_connectome.mat')
+load('~/Research/data/EM/c_elegans/ConnOrdered_040903.mat')
 
 [times,errors,iters]=bench_qap(A_init_t_ordered);
 
@@ -9,7 +10,7 @@ load('~/Research/data/EM/c_elegans/c_elegans_chemical_connectome.mat')
 [median(iters); mad(iters)]
 
 
-[times,errors,iters]=bench_qap(Ac);
+[times,errors,iters]=bench_qap(Ag_t_ordered);
 
 [median(times); mad(times)]
 [median(errors); mad(errors)]
